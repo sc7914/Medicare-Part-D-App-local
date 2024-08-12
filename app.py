3be595cd4e9e5b78547301b7644ebe38079ed53d
Susan Chor <sc7914@stern.nyu.edu>
2024-08-12 18:56:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
+!pip install flask-ngrok
+from flask_ngrok import run_with_ngrok
 from flask import Flask
-app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app = Flask(__name__)
+run_with_ngrok(app)   #starts ngrok when the app is run
+@app.route("/")
+def home():
+    return
+
+!ngrok http 5000  # Replace 5000 with your Flask app's port
+
+from flask import Flask
+from flask_ngrok import run_with_ngrok
+
+app = Flask(__name__)
+run_with_ngrok(app)   #starts ngrok when the app is run
+
+@app.route("/")
+def home():
+    return "<p>Hello, World!</p>"
+
+app.run()
+
+if __name__ == '__main__':
+    app.run(debug=True)
